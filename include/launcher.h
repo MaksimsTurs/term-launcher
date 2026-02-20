00e0704e984c5972cb868c1b93174c71562f593c
MaksimsTurs <maksims07turs@gmail.com>
2026-02-20 11:52:36 +0000

    style(launcher.c): remove all leading tabs

--- include/launcher.h
+++ include/launcher.h
@@ -1,35 +1,38 @@
 #ifndef H_LAUNCHER
-	#include "input.h"
-	#include "render.h"
-	#include "app.h"
-	#include "utility.h"
 
-	#include <stdlib.h>
-	#include <unistd.h>
-	#include <signal.h>
+#include "input.h"
+#include "render.h"
+#include "app.h"
+#include "utility.h"
 
-	typedef struct s_launcher_state {
-		unsigned char 		is_running;
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
-		t_input_buffer		searched_app;
+typedef struct s_launcher_state {
+	unsigned char 		is_running;
+
+	t_input_buffer		searched_app;
 		
-		int				     		selected_app;
+	int				     		selected_app;
 		
-		t_apps				 		usr_apps;
-		t_apps 						finded_apps;
-	} t_launcher_state;
+	t_apps				 		usr_apps;
+	t_apps 						finded_apps;
+} t_launcher_state;
 
-	// Core functions
-	void launcher_init(int argc, const char** argv);
-	void launcher_run();
-	void launcher_destroy();
+// Core functions
+void launcher_init(int argc, const char** argv);
+void launcher_run();
+void launcher_destroy();
 
-	void launcher_render();
-	void launcher_process_input();
-	void launcher_process_data();
+void launcher_render();
+void launcher_process_input();
+void launcher_process_data();
 
-	void launcher_run_app();
-	// Keyboard event.
-	void					launcher_cursor_up();
-	void					launcher_cursor_down();
+void launcher_run_app();
+
+// Keyboard event.
+void					launcher_cursor_up();
+void					launcher_cursor_down();
+
 #endif
